e99371d443a04338ed23a56b78929dffe7c6b0ec
pDorian <dorianparet@gmail.com>
2019-09-11 17:01:04 +0200

    ajout du constructeur pour rentré du nom et prenom de l'élève

---
+++ eleve.cpp
@@ -0,0 +1,9 @@
+Eleve::Eleve(){
+	cout<<"Veuillez rentrer le nom et le prenom de l'élève"<<endl<<endl;
+	cout<<"Le nom ?"<<endl;
+	string nom;
+	getline(cin,nom);
+	cout<<"Le prenom ?"<<endl;
+	string prenom;
+	getline(cin,prenom);	
+}
---
+++ eleve.h
@@ -0,0 +1,7 @@
+class Eleve{
+	private:
+		string NomEleve;
+		string PrenomEleve;
+	public:
+		Eleve(string nom, string prenom);
+};
